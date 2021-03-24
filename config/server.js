fcbda0f2b71f8e6fdd7f7840180d57c4fe7ddb16
RafaelDoering <rafael.doering.soares@usp.br>
2021-03-23 21:49:56 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a428513c43c7407bb2facba1079551e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac6eebdd-33d7-41a5-ac3b-17964c56b22d'
+};
